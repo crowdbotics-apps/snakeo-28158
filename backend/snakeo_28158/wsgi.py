bf40d6a2c63b2b36675301a3babdc4a743816221
Crowdbotics <team@crowdbotics.com>
2021-06-22 00:40:23 +0000

    Update models

--- backend/snakeo_28158/wsgi.py
+++ backend/snakeo_28158/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snakeo_28158.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snakeo_28158.settings")
 
 application = get_wsgi_application()
